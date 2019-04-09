2b98c3e0428aff909db476319867367870795b1b
EDILBER GOMEZ <edilbergomez@hotmail.com>
2019-04-08 22:26:34 -0500

    creacion carpeta y proy

---
+++ 2019tbg/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="_2019tbg.MvcApplication" Language="C#" %>
---
+++ 2019tbg/Models/jodido/jodido.aspx.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace _2019tbg.Models.jodido
+{
+	public partial class jodido : System.Web.UI.Page
+	{
+		protected void Page_Load(object sender, EventArgs e)
+		{
+
+		}
+	}
+}
